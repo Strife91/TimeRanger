30715bb4b1235f006511b5bb4b03a3d9a7219504
Gattanjo Nilsson <gattanjo.nilsson@hotmail.com>
2024-11-06 10:46:03 +0900

    Initial Commit

---
+++ TimeRanger/TimeRangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeRangerApp.swift
+//  TimeRanger
+//
+//  Created by Gattanjo Nilsson on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct TimeRangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeRangerTests/TimeRangerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeRangerTests.swift
+//  TimeRangerTests
+//
+//  Created by Gattanjo Nilsson on 2024-11-06.
+//
+
+import Testing
+@testable import TimeRanger
+
+struct TimeRangerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
